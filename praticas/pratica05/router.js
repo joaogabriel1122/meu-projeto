f7651b0f04a69ea09e1177bd81da31cb6f0dfeb5
joao gabriel <rochagabriel1122@gmail.com>
2024-03-14 22:10:03 -0300

    pratica 05 atualização

---
+++ praticas/pratica05/router.js
@@ -0,0 +1,29 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get("/produtos", function(req, res){
+   res.json([]);
+}); 
+
+router.get("/produtos/:produtoId", function(req, res){
+    if (req.params.produtoId == 1)res.json({})
+    else res.status(404).json ({msg:"produto não encontrado"})
+
+});
+
+router.post("/produtos", function(req, res) {
+
+});
+
+router.put("/produtos/:produtoId", function(req, res){
+
+});
+
+router.delete("/produtos/:produtoId", function(req, res){
+
+});
+
+module.exports = router;
+
+
